8742ce4a69f18b27c28fc41e73d7ea9150dc046a
Tan Jun An <junantan318@gmail.com>
2026-07-14 21:12:29 +0800

    configure fastapi proxy root

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,30 +1,32 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.routers import users,auth, categories, expenses
+from app.routers import users, auth, categories, expenses
 from app.database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
+app = FastAPI(root_path="/api")
 
-app = FastAPI()
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://expense-tracker-one-delta-41.vercel.app",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(categories.router)
 app.include_router(expenses.router)
+
 
 @app.get("/")
 async def read_root():
